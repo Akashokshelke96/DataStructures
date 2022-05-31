e1321bd6e20a4a738798f77cd0b7b4b78ae4ac50
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-06-01 00:00:29 +0530

    learning CalculateStackHeight power of N

---
+++ src/com/sourceCode/recursion/CalculateStackHeight.java
@@ -0,0 +1,19 @@
+package com.sourceCode.recursion;
+
+public class CalculateStackHeight {
+    public static int calcPower(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        if(n==0){
+            return 1;
+        }
+        int calc_powerNm1 = calcPower(x,n-1); // calculating power for n-1
+        int calc_powerN = x * calc_powerNm1; //power of n * power of n-1
+        return calc_powerN;
+    }
+    public static void main(String[] args) {
+        int x = 2 , n = 5;
+        System.out.println(calcPower(x,n));
+    }
+}
