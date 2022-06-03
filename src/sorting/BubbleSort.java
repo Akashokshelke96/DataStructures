de6560a709fd1f4486daa03c8f1b1381ab4b50ef
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-06-03 22:12:17 +0530

    different approach

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -6,16 +6,16 @@ public class BubbleSort {
         //implementation of BUBBLE SORT
         int[] arr = {7,8,3,1,2};
         //time complexity = O(n^2)
-        for(int i = 0 ; i < arr.length -1; i++){
-            for(int j = 0 ; j< arr.length - 1- i; j++){//the last element need not be compared again as it is the heaviest. therefore n-1-i
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }printArray(arr);
+       for(int i = 0 ; i < arr.length - 1 ; i++){
+           for(int j = 0 ; j < arr.length-1-i; j++){
+               if(arr[j]> arr[j+1]){
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+               }
+           }
 
+       }printArray(arr);
 
     }
     public static void printArray(int[] arr){
