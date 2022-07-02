81c387fbd554178c1e8fa4238a1144f44845dc93
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-07-02 17:44:35 +0530

    corrections commit

--- src/binarySearch/OrderAgnosticBinarySearch.java
+++ src/binarySearch/OrderAgnosticBinarySearch.java
@@ -4,9 +4,9 @@ public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
         //binary search for both increasing and decreasing order
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
-        System.out.println(binarySearch(arr,8));
+        System.out.println(OrderAgnosticBinarySearch(arr,8));
     }
-    static int binarySearch(int[] arr , int target) {
+    static int OrderAgnosticBinarySearch(int[] arr , int target) {
         int start = 0;
         int end = arr.length - 1;
         //find whether array is sorted ascending or descending
